1f189f4fe35a026fbdda52456516ab05544a4e0f
JacksonZ03 <60581068+JacksonZ03@users.noreply.github.com>
2024-02-21 11:24:39 +0000

    added the nodeport services ports to control-panel node setup in case it is used as a workernode when taint is off

--- control-plane_server_setup.sh
+++ control-plane_server_setup.sh
@@ -19,6 +19,7 @@ sudo apt update -y && sudo apt upgrade -y
 sudo ufw allow 6443/tcp # k8s API server
 sudo ufw allow 8443/tcp # k8s API server
 sudo ufw allow 2379:2380/tcp # etcd server client API
+sudo ufw allow 30000:32767/tcp # k8s NodePort Services
 sudo ufw allow 10250/tcp # k8s Kubelet API
 sudo ufw allow 10259/tcp # kube-scheduler
 sudo ufw allow 10257/tcp # kube-controller-manager
